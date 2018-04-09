ed073963e46648d8a29c36e273bbb726dc8d4093
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-04-09 11:03:29 -0300

    add user

--- configUser.sh
+++ configUser.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-	for u in `cut -d: -f1 /etc/passwd`; do \
-		mkdir /home/$(u)/.desafios -p ;\
-		>/home/$(u)/.desafios/questsAccepted ;\
-	done
-
-	for u in `cut -d: -f1 /etc/passwd`; do \
-		>/home/$(u)/.desafios/questsDone;\
+	for u in `ls /home/`; do 
+		mkdir /home/$u/.desafios -p ;
+		>/home/$u/.desafios/questsAccepted ;
+		>/home/$u/.desafios/questsDone;
 	done
 	
