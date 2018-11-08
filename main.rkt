04172b31a0a1a139473865eda457a30b28a0bdc8
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-11-08 21:57:43 -0200

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -1,4 +1,12 @@
 #lang racket
 
+(require "user.rkt")
+(require "shell.rkt")
+
+(define (system-message str)
+  (string-append "" str ""))
+
+
 (define (loop)
-  (
+  (let ([line (parser (read-line))])
+    (if (
