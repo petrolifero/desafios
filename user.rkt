89a607ea5c3bd3a3b0f8f053c7ca92330ddfedb5
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-11-09 00:33:44 -0200

    Update user.rkt

--- user.rkt
+++ user.rkt
@@ -1,12 +1,14 @@
 #lang racket
 
+(require "shell.rkt")
+
 (provide (all-defined-out))
 
 (struct user (name level) #:transparent)
 
 (define (create-user name)
   (let* ([home (format "/home/~a" name)]
-         [config-file (format "~a/.desafios.user" home]
+         [config-file (format "~a/.desafios.user" home)]
          [level (call-with-input-file config-file (lambda (in) (port->string in)))])
          (user name level)))
 
@@ -16,5 +18,7 @@
     (level-allow? (user-level user) command)))
     
 (define (quests user)
-  (let ([str ()])
-    (string-strip str " ")))
+  (let* ([home (format "/home/~a" (user-name user))]
+         [config-file (format "~a/.desafios.quests" home)]
+         [quests (call-with-input-file config-file (lambda (in) (string-split (port->string in) "\n")))])
+         quests))
