10047ed99dc18fa225a9fd8447990f60db28349b
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-11-09 11:33:30 -0200

    Update entrance.c

--- entrance.c
+++ entrance.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include <unistd.h> // for pipe
+#include <unistd.h> // for pipe and fork
+
+#define WRITETO 1
+#define READFROM 0
 
 int main(int argc, char* argv[])
 {
   int c2racket[2];
   int racket2c[2];
+  int childPid;
   pipe(c2racket);
   pipe(racket2c);
+  childPid=fork();
   return 0;
 }
