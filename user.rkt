088915859eb1d1e6afd30e82ac1c97148e5bfe43
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-11-08 23:26:40 -0200

    Update user.rkt

--- user.rkt
+++ user.rkt
@@ -5,4 +5,10 @@
 (struct user (name level) #:transparent)
 
 (define (can-execute? user command)
-  (
+  (and
+    (quest-allow? (quests user) command)
+    (level-allow? (user-level user) command)))
+    
+(define (quests user)
+  (let ([str ()])
+    (string-strip str " ")))
