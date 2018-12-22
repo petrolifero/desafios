51648971a5ff7c0f836c18b383f5e2b6ed3ea0ac
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-12-22 01:21:59 -0200

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -22,47 +22,44 @@
 
 (current-directory home)
 
-(system-message "you are in the tutorial, inside your personal directory. Start by pressing ls with enter to view the contents of your home folder")
+(system-message "you are in the tutorial, inside your personal directory. Start by pressing ls with enter to view the contents of your home folder\n")
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
 	(if (equal? x "ls")
 		(begin
 		  	(command-message (red (map path->string (directory-list (current-directory)))))
-			(system-message (format "Olha só o conteudo do seu diretorio. Reparou no textinho antes de você digitar ls? Ele mostra que você se chama ~a e esta atualmente dentro do diretorio(um nome pomposo para pasta) ~a\n" user (current-directory))))
+			(system-message (format "Look at the contents of your directory. Did you notice the text before you type ls? It shows that you are calling ~a and it is currently inside the directory (a pompous folder name) ~a \n" user (current-directory))))
 		(begin
-		  	(system-message (format "Não não. Você digitou ~a, era para ter digitado ls. Por favor, tente de novo.\n" x))
+			(system-message (format "No. You typed ~a, it was to have typed ls. Please try again. \n" x))
 			(display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 			(loop (read-line)))))
 
-
-(system-message "Agora vamos criar um diretorio para você. Pode escolher o nome. Se quiser criar desafios, digite \"mkdir desafios\"\n")
+(system-message "Now let's create a directory for you. You can choose the name. If you want to create challenges, type \"mkdir challenges\"\n")
 
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
 	(if (string-prefix? x "mkdir")
 		(begin
 		  	(make-directory* (car (string-split x "mkdir ")))
-			(system-message (format "Olha só, seu diretorio foi criado\n")))
+			(system-message (format "Look, your directory was created\n")))
 		(begin
-		  	(system-message (format "Não não. Você digitou ~a, era para ter digitado algum mkdir. Por favor, tente de novo.\n" x))
+		  	(system-message (format "No. You typed ~a, it was to have typed some mkdir. Please try again.\n" x))
 			(display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 			(loop (read-line)))))
-
-(system-message "Você criou o diretorio que especificou... Será mesmo??\n")
-(system-message "Não confie em mim. Dê um \"ls nome do diretorio\" para verificar\n")
+(system-message "You have created the directory that you specified ... Will it really ??\n")
+(system-message "Do not trust me. Give \"ls name-of-directory\" to verify\n")
 
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
 	(if (string-prefix? x "ls")
 		(begin
 		  	(command-message (red (map path->string (directory-list (car (string-split x "ls "))))))
-			(system-message (format "Olha só o conteudo do seu diretorio novo... Ué, sem nada? Relaxa, ela só esta vazia. Agora tem sua nova pasta.\nSe ela não existisse, ia aparecer uma mensagem de erro. \n")))
+			(system-message (format "Look at the contents of your new directory ... Wow, nothing? Relax, it's just empty. You now have your new folder.\nIf it did not exist, an error message would appear.\n")))
 		(begin
-		  	(system-message (format "Não não. Você digitou ~a, era para ter digitado ls. Por favor, tente de novo.\n" x))
+		  	(system-message (format "No. You typed ~a, it was to have typed ls. Please try again.\n" x))
 			(display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 			(loop (read-line)))))
-
-(system-message "Agora vamos entrar nesse diretorio e fazer as quests ali. Para viajar pelos diretorios usamos o comando cd. digite \"cd nome-do-seu-diretorio\" para entrar nessa pasta.")
+(system-message "Now let's go into that directory and do the quests there. To travel through the directories we use the cd command. Type \"cd your-directory-name \" to enter this folder.\n")
 
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
@@ -70,20 +67,20 @@
 		(begin
 		  	(current-directory (car (string-split x "cd "))))
 		(begin
-		  	(system-message (format "Não não. Você digitou ~a, era para ter digitado cd com algum diretorio. Por favor, tente de novo.\n" x))
+		  	(system-message (format "No. You typed ~a, it was to have typed cd with your directory. Please try again.\n" x))
 			(display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 			(loop (read-line)))))
 
 
-(system-message "Olhe só o que você fez : Note a mudança\n")
-(system-message "quando notar, digite next e você oficialmente saiu do tutorial.A partir de agora, não terão mais tantas mensagens do sistema, vermelhas assim para te guiar, você estará mais livre. Não se amedronte, a primeira quest da nova vida será sobre o manual para pedir ajuda :)\n")
+(system-message "Look what you've done: Note the change\n")
+(system-message "when you notice, type next and you officially left the tutorial. From now on, you will not have as many system messages, so red to guide you, you will be more free. Do not be frightened, the first quest of the new life will be about the manual to ask for help :)\n")
 
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
 	(if (equal? x "next")
 	    (void)
 	    (begin
-	      (system-message "Não não, digite next \n")
+	      (system-message "No, type next\n")
 	      (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 	      (loop (read-line)))))
 
@@ -100,8 +97,7 @@
 
 ;;; como mostrar as quests disponiveis??
 
-
-(system-message "Bem-vindo ao level 1.\n")
+(system-message "Welcome to level 1.\n")
 (system-message "")
 (display (format "~a@desafios:~a " (getenv "USER") (current-directory)))
 (let loop ((x (read-line)))
