2981ddcf006ebcf347ac7b4b3034b381d9ddea34
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-12-30 03:23:46 -0200

    modified quests

--- quests.rkt
+++ quests.rkt
@@ -2,6 +2,13 @@
 
 (require "shell.rkt");
 
+(struct idt (a) #:transparent);
+
+;(string (listOf Idt) Command . -> . Quest)
+(struct quest (name pre c) #:transparent);
+
+
+
 (define (parser-quest port)
   (peg top-quest (port->string port)));
 
@@ -10,10 +17,13 @@ _ < [ \t\n]* ;
 sep < [ \t]* ;
 EOI < ! . ;
 
-top-quest <- _ quest _ EOI ;
-quest <- _ 'quest' _ identifier _ preRequisites? _ comand _ 'tseuq' _ ;
-identifier <- [a-zA-Z]+ ;
-preRequisites <- '<' _ identifier _ (',' _ identifier _)*;
+top-quest <- _ v:quest _ EOI -> v;
+quest <- quest-with-prerequisites / quest-without-prerequisites ;
+quest-with-prerequisites <- _ 'quest' _ v1:identifier _ v2:preRequisites _ v3:comand _ 'tseuq' _ -> (quest v1 v2 v3);
+quest-without-prerequisites <- _ 'quest' _ v1:identifier _ v2:comand _ 'tseuq' _ -> (quest v1 '("tutorial") v2);
+
+identifier <- v:[a-zA-Z]+ -> (idt v);
+preRequisites <- '<' _ v:(identifier _ (~',' _ identifier _)*) -> v;
 comand <- comandUnit (_ comandUnit)* ;
 comandUnit <- (exec / echo-quest / file) ';' ;
 exec <- 'exec' sep shell ;
